29e31a6935579326d24c424ed6b592cf03f967c7
Hyunmin Lee <wellmin11@naver.com>
2018-05-23 20:42:07 +0900

    Add button handler skeletons

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,4 +1,43 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
 public class Controller {
+    private Alert alert;
+
+    @FXML
+    protected void l_load_file(ActionEvent event){
+        System.out.println("left_load_file click!");
+    }
+
+    @FXML
+    protected void l_edit_file(ActionEvent event){
+        System.out.println("left_edit_file click!");
+    }
+
+    @FXML
+    protected void l_save_file(ActionEvent event){
+        System.out.println("left_save_file click!");
+    }
+
+    @FXML
+    protected void r_load_file(ActionEvent event){
+        System.out.println("right_load_file click!");
+    }
+
+    @FXML
+    protected void r_edit_file(ActionEvent event){
+        System.out.println("right_edit_file click!");
+    }
+    @FXML
+    protected void r_save_file(ActionEvent event){
+        System.out.println("right_save_file click!");
+    }
+
+    @FXML
+    protected void compare_file(ActionEvent event){
+        System.out.println("compare_file click!");
+    }
 }
