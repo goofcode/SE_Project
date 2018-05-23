5579ac35c01aa765028a3d90ef4208764c83ee25
Holquew <holquew@gmail.com>
2018-05-23 21:03:18 +0900

    First Model .java file

---
+++ src/model/Model.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Model {
+    // 왼쪽이 0 오른쪽이 1
+    File[] files = new File[2];
+
+    ArrayList<String> leftText = new ArrayList<String>();
+    ArrayList<String> rightText = new ArrayList<String>();
+
+    public void loadFile(File file, int side) {
+        files[side] = file;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                switch (side){
+                    case 0:
+                        leftText.add(line);
+                        break;
+                    case 1:
+                        rightText.add(line);
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveFile(){
+
+    }
+
+    public File getFile(int side){
+        return files[side];
+    }
+
+    public ArrayList<String> getLeftText(){
+        return leftText;
+    }
+
+    public ArrayList<String> getRightText(){
+        return rightText;
+    }
+
+}
