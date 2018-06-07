1c7cd88511077238df18a9ee4807bf60d86cf009
Yoonjin Han <holquew@gmail.com>
2018-06-07 16:45:03 +0900

    Make test folder

---
+++ unit_test/model/FileManagerTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileManagerTest {
+
+    @Test
+    public void synchronizeSize() {
+    }
+
+    @Test
+    public void getLinesAsOneString() {
+    }
+
+    @Test
+    public void setLines() {
+    }
+
+    @Test
+    public void setLine() {
+    }
+
+    @Test
+    public void setLinesFromOneString() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void clearText() {
+    }
+}
