6b1bc84d6ed6b58c0c9b4c026298c046a612909b
Yoonjin Han <holquew@gmail.com>
2018-06-07 21:14:17 +0900

    Make junit test file for FileManager

--- src/test/model/FileManagerTest.java
+++ src/test/model/FileManagerTest.java
@@ -1,36 +1,55 @@
 package model;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
 
 public class FileManagerTest {
 
-    @Test
-    public void synchronizeSize() {
+    private FileManager fileManagerA;
+    private FileManager fileManagerB;
+
+    @BeforeClass
+    public static void setup(){
+
+        System.out.println("Start");
+    }
+
+    @Before
+    public void createFile() throws URISyntaxException {
+
+        fileManagerA = new FileManager(new File ("test_a.txt"));
+        fileManagerB = new FileManager(new File ("test_b.txt"));
     }
 
     @Test
-    public void getLinesAsOneString() {
+    public void synchronizeSizeTest() {
+
+        fileManagerB.synchronizeSize(fileManagerA);
+        assertEquals(6, fileManagerB.getLines().size());
     }
 
-    @Test
-    public void setLines() {
-    }
 
     @Test
-    public void setLine() {
+    public void getLinesAsOneStringTest() {
+
+        String confirmString = fileManagerA.getLinesAsOneString();
+        assertEquals("asdf\nasdf\nasdf\nasdf\nasdf", confirmString);
     }
 
-    @Test
-    public void setLinesFromOneString() {
-    }
 
     @Test
-    public void save() {
+    public void setLinesFromOneStringTest() {
+
+        fileManagerB.setLinesFromOneString("Hello\nWorld\nGoodbye\nMy\nLife");
+
+        assertEquals(5, fileManagerB.getLines().size());
+        assertEquals("Hello", fileManagerB.getLine(0));
     }
 
-    @Test
-    public void clearText() {
-    }
 }
